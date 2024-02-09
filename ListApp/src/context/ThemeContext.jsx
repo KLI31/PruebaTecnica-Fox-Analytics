2f5a4972bb465c37bf0e271a5d11617d1113b44f
Luis David <lrambao5432@gmail.com>
2024-02-09 09:33:17 -0500

    :sparkles: Context to themeChange

---
+++ ListApp/src/context/ThemeContext.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect, createContext } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
