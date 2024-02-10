32fd13fe52988d5dd61a608801f3ca9bf20c7bca
Luis David <lrambao5432@gmail.com>
2024-02-10 11:21:59 -0500

    :recycle: Add new functionality

--- NewsApiApp/api/globalApi.js
+++ NewsApiApp/api/globalApi.js
@@ -6,9 +6,18 @@ const api = create({
 
 const apiKey = "?country=us&apiKey=fd518460fde14c659be315e825a6e3fc";
 
+// Conseguir las noticias principales
 const getTopHeadlines = api.get(`/top-headlines${apiKey}`);
 
-const getNewsByCategory = (category) =>
-  api.get(`/everything?q=${category}&apiKey=fd518460fde14c659be315e825a6e3fc`);
+// &apiKey=5ffd4be6fd4d41f2a3b678e7d7a19f40
+// &apiKey=fd518460fde14c659be315e825a6e3fc
 
-export default { getTopHeadlines, getNewsByCategory };
+// Conseguir las noticias por categoria
+const getNewsByCategory = (category) =>
+  api.get(`/everything?q=${category}&apiKey=5ffd4be6fd4d41f2a3b678e7d7a19f40`);
+
+// Conseguir las noticias por busqueda
+const getNewsBySearch = (search) =>
+  api.get(`/everything?q=${search}&apiKey=5ffd4be6fd4d41f2a3b678e7d7a19f40`);
+
+export default { getTopHeadlines, getNewsByCategory, getNewsBySearch };
